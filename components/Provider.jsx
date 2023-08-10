39cd910bce041b1b06b0c507ab5f2308f844998f
Albert Devasia <albertdevasia2024@mca.ajce.in>
2023-08-10 14:39:40 +0530

    version 4

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,13 +1,20 @@
+'use client'
+
 import React from 'react'
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
 
 
 const Provider = ({ children , session }) => {
   return (
     <SessionProvider session = {session}>
+      <ComponentUsingUseSession />
       {children}
       </SessionProvider>
   )
+}
+
+function ComponentUsingUseSession (){
+  const {data: session, status} = useSession()
 }
 
 export default Provider
